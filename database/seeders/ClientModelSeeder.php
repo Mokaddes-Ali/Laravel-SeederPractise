7e2b1efd3fd13ae2ba2755042023f73d55d76708
mokadde <mokaddes.ru2000@gmail.com>
2024-08-28 19:23:33 +0600

    commit

---
+++ database/seeders/ClientModelSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ClientModelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
